b983dd46c92a11c79cb2a20473f35361781e6f4a
soaibsafi <soaib.safi@gmail.com>
2026-06-26 13:58:39 +0200

    Updated execution logic with alloy redundancy and explanation

---
+++ frontend/tools/alloy/features/index.ts
@@ -0,0 +1,4 @@
+export * from './redundancy/redundancyAtoms';
+export * from './redundancy/redundancyApi';
+export * from './redundancy/redundancyCodeLens';
+export * from './redundancy/RedundancySummary';
